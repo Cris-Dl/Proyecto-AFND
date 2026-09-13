c8ee2789f267b5e112cd3e818d87a4346d2baa4e
Marvin7715 <vasquezmarvin588@gmail.com>
2026-09-13 14:39:20 -0600

    en producto.py se agregaron 5 productos para poder realizar pruebas con la logica de gestor_productos.py

---
+++ productos/producto.py
@@ -0,0 +1,37 @@
+productos = [
+    {
+        "id": 1,
+        "nombre": "Laptop",
+        "precio": 5500.00,
+        "categoria": "Tecnología",
+        "existencia": 5
+    },
+    {
+        "id": 2,
+        "nombre": "Mouse",
+        "precio": 150.00,
+        "categoria": "Accesorios",
+        "existencia": 10
+    },
+    {
+        "id": 3,
+        "nombre": "Teclado",
+        "precio": 300.00,
+        "categoria": "Accesorios",
+        "existencia": 0
+    },
+    {
+        "id": 4,
+        "nombre": "Audífonos",
+        "precio": 200.00,
+        "categoria": "Audio",
+        "existencia": 8
+    },
+    {
+        "id": 5,
+        "nombre": "Monitor",
+        "precio": 1800.00,
+        "categoria": "Tecnología",
+        "existencia": 4
+    }
+]
